a22a9d0d19fcc57d6880f4a085272b9dfa300ac4
Hodonou SOUNTON <sounton@gmail.com>
2017-10-01 12:05:12 +0100

    OK

--- users/controllers/users.js
+++ users/controllers/users.js
@@ -4,17 +4,19 @@ import User from '../models/users'
 import getPostUser from '../config/getUser'
 import cfg from '../config/config'
 
-
-exports.all = () => {
-  console.log('BEGIN ROUTES :::: OK')
-  passport.authenticate('jwt', { session: false })
-}
-
 exports.register = async (req, res) => {
   try {
     res.status(201).json(await getPostUser(req).save())
   } catch (err) {
     res.status(500).json({ error: err.message })
+  }
+}
+
+exports.list = async (req, res) => {
+  try {
+    res.status(200).json(await User.find())
+  } catch (err) {
+    res.status(404).json({ error: err.message })
   }
 }
 
--- users/routes/users.js
+++ users/routes/users.js
@@ -5,16 +5,9 @@ import ctrl from '../controllers/users'
 const router = express.Router()
 
 router.post('/', passport.authenticate('jwt', { session: false }), ctrl.register)
+router.get('/list', passport.authenticate('jwt', { session: false }), ctrl.list)
 router.post('/login', ctrl.login)
 
 
 module.exports = router
 
-// module.exports = (app) => {
-//   app.route('/gobl/v1/users')
-//     .all(ctrl.all)
-//     .post(ctrl.register)
-
-//   app.route('/gobl/v1/users/login')
-//     .post(ctrl.login)
-// }
